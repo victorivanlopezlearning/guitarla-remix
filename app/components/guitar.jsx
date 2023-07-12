4a161dc8a66be276e7b9760fb112d4ecbf466cf7
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-07-12 14:52:38 -0600

    Add: store styles

---
+++ app/components/guitar.jsx
@@ -0,0 +1,28 @@
+import { Link } from "@remix-run/react";
+
+function Guitar({ guitar }) {
+  
+  const { name, price, description, image, url } = guitar;
+  const imageURL = image.data.attributes.formats.small.url;
+
+  return (
+    <div className="guitar">
+      <img src={imageURL} alt={`Imagen guitarra ${name}`} />
+
+      <div className="guitar__content">
+        <h2 className="guitar__name">{name}</h2>
+        <p className="guitar__description">{description}</p>
+        <p className="guitar__price">${price} MXN</p>
+
+        <Link
+          className="guitar__link"
+          to={`/guitars/${url}`}
+        >
+          Ver Producto
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Guitar;
--- app/routes/store.jsx
+++ app/routes/store.jsx
@@ -1,13 +1,48 @@
+import { useLoaderData } from "@remix-run/react";
 import { getGuitars } from "~/models/guitars.server";
+import Guitar from "~/components/guitar";
+import styles from '~/styles/store.css';
+
+export function meta() {
+  return [
+    { title: 'GuitarLA - Explora nuestra tienda en línea: Guitarras de calidad' },
+    { name: 'description', content: 'Descubre la mejor selección de marcas reconocidas y sumérgete en un mundo de posibilidades musicales. Nuestro objetivo es proporcionarte una experiencia de compra excepcional, con opciones para todos los niveles de habilidad y estilos musicales.'}
+  ];
+}
+
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles,
+    },
+  ];
+};
 
 export async function loader() {
   const guitars = await getGuitars();
-  return guitars;
+  return guitars.data;
 }
 
 function Store() {
+
+  const guitars = useLoaderData();
+
   return (
-    <h1>Store</h1>
+    <main className="store container">
+      <h1 className="heading">Nuestra Colección</h1>
+
+      {guitars?.length && (
+        <div className="store__content">
+          {guitars?.map( guitar => (
+            <Guitar
+              key={ guitar?.id }
+              guitar={ guitar?.attributes }
+            />
+          ))}
+        </div>
+      )}
+    </main>
   )
 }
 
