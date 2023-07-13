a6c61f7b63418e55b0a0e5c0220f6ef230ce03c9
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-07-13 13:15:54 -0600

    Add: formatDate function

--- app/components/post.jsx
+++ app/components/post.jsx
@@ -1,15 +1,19 @@
 import { Link } from "@remix-run/react";
+import { formatDate } from "../utils/formatDate";
 
 function Post({ post }) {
   const { title, content, image, url, publishedAt } = post;
+
   const imageURL = image.data.attributes.formats.small.url;
+  const date = formatDate(publishedAt);
+  
   return (
     <article className="post">
       <img src={imageURL} alt={`Imagen portada ${title}`} />
 
       <div className="post__content">
         <h2 className="post__title">{title}</h2>
-        <p className="post__meta">{publishedAt}</p>
+        <p className="post__meta">{date}</p>
         <p className="post__extract">{content}</p>
         <Link
           className="post__link"
---
+++ app/utils/formatDate.js
@@ -0,0 +1,17 @@
+
+/**
+ * Date formatting
+ * @param {Number|String} date
+ * @returns {String}
+ */
+export const formatDate = ( date ) => {
+  const objDate = new Date(date);
+
+  const options = {
+      year: 'numeric',
+      month: 'long',
+      day: '2-digit'
+  };
+
+  return objDate.toLocaleDateString('es-MX', options);
+}
