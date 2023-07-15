1cd2bed49fc46e1e9b14076092d17ce8d7480f2a
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-07-14 19:42:46 -0600

    Updated: global meta

--- app/root.jsx
+++ app/root.jsx
@@ -13,14 +13,6 @@ import styles from '~/styles/index.css';
 import Header from '~/components/header';
 import Footer from '~/components/footer';
 
-export function meta() {
-  return [
-    { charset: "utf-8" },
-    { title: "GuitarLA" },
-    { name: "viewport", content: "width=device-width,initial-scale=1" },
-  ];
-}
-
 export function links() {
   return [
     {
@@ -59,6 +51,11 @@ function Document({ children }) {
   return (
     <html lang="en">
     <head>
+      <meta charSet="utf-8" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+      />
       <Meta />
       <Links />
     </head>
