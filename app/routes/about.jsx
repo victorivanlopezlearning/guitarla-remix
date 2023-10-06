6791b493ef20f4d00a1f56b0f19665569c7b22d2
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-05 23:43:03 -0600

    Updated: description

--- app/routes/about.jsx
+++ app/routes/about.jsx
@@ -31,8 +31,8 @@ function About() {
         <img className='about__image' src={ imageAbout } alt="Imagen Nosotros" />
 
         <div className='about__description'>
-          <p>Donec consectetur enim quis sagittis volutpat. Duis ut nisl nec justo iaculis aliquam a at felis. Nullam lectus velit, sagittis vel pellentesque non, molestie in augue. In bibendum dui sed ante scelerisque faucibus. Nulla velit purus, rutrum vel felis nec, aliquam venenatis odio. Duis ut dignissim diam. Donec vel nunc velit.</p>
-          <p>Donec consectetur enim quis sagittis volutpat. Duis ut nisl nec justo iaculis aliquam a at felis. Nullam lectus velit, sagittis vel pellentesque non, molestie in augue. In bibendum dui sed ante scelerisque faucibus. Nulla velit purus, rutrum vel felis nec, aliquam venenatis odio. Duis ut dignissim diam. Donec vel nunc velit.</p>
+          <p>Somos un equipo de entusiastas de la música y apasionados por las guitarras, comprometidos con brindarte una experiencia excepcional. Nuestra misión es simple pero profunda: proporcionar guitarras y equipo de la más alta calidad para inspirar y elevar tu música.</p>
+          <p>En GuitarLA, la música y las guitarras son más que un negocio; son nuestra pasión y nuestro compromiso. Desde nuestros modestos comienzos, hemos crecido para convertirnos en un destino en línea confiable para amantes de la música. Nuestra dedicación a la excelencia. Desde la selección cuidadosa de guitarras de marcas reconocidas hasta el servicio al cliente de primera clase.</p>
         </div>
       </div>
     </main>
